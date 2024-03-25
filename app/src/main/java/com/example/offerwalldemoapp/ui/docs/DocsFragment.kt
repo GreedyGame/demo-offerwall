b7d1d91c8255fafd20a93097d300504a2e1a4394
greedyjustin <justin@greedygame.com>
2024-03-25 17:46:40 +0530

    major: added webview for docs

--- app/src/main/java/com/example/offerwalldemoapp/ui/docs/DocsFragment.kt
+++ app/src/main/java/com/example/offerwalldemoapp/ui/docs/DocsFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.offerwalldemoapp.databinding.FragmentDocsBinding
 
 class DocsFragment : Fragment() {
     private lateinit var mBinding: FragmentDocsBinding
+    private val mWebViewClient = object : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            mBinding.progressBar.isVisible = false
+        }
+    }
 
     companion object {
         fun newInstance() = DocsFragment()
@@ -19,5 +28,18 @@ class DocsFragment : Fragment() {
     ): View {
         mBinding = FragmentDocsBinding.inflate(layoutInflater)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(mBinding) {
+            progressBar.isVisible = true
+            with(webView) {
+                webViewClient = mWebViewClient
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                loadUrl("https://pubscale.gitbook.io/offerwall-sdk")
+            }
+        }
     }
 }
