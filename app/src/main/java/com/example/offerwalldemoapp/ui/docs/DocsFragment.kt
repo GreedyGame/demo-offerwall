57c355b34f3be6c2698128af7dea6c260957c7f0
greedyjustin <justin@greedygame.com>
2024-03-27 15:39:36 +0530

    major: added swipe to refresh function to docs screen

--- app/src/main/java/com/example/offerwalldemoapp/ui/docs/DocsFragment.kt
+++ app/src/main/java/com/example/offerwalldemoapp/ui/docs/DocsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -15,23 +16,41 @@ import androidx.fragment.app.activityViewModels
 import com.example.offerwalldemoapp.databinding.FragmentDocsBinding
 import com.example.offerwalldemoapp.ui.dashboard.MainViewModel
 
-
 class DocsFragment : Fragment() {
     private lateinit var mBinding: FragmentDocsBinding
     private val mActivityViewModel by activityViewModels<MainViewModel>()
-    private val mWebViewClient = object : WebViewClient() {
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-            mBinding.progressBar.isVisible = false
-        }
+    private val mWebViewClient by lazy {
+        object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                hideLoader()
+            }
 
-        override fun shouldOverrideUrlLoading(
-            view: WebView?,
-            request: WebResourceRequest?
-        ): Boolean {
-            val intent = Intent(Intent.ACTION_VIEW, request!!.url)
-            context?.startActivity(intent)
-            return true
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val intent = Intent(Intent.ACTION_VIEW, request!!.url)
+                context?.startActivity(intent)
+                return true
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+                hideLoader()
+            }
+        }
+    }
+
+    private fun hideLoader() {
+        with(mBinding) {
+            progressBar.isVisible = false
+            swipeToRefresh.isVisible = true
+            swipeToRefresh.isRefreshing = false
         }
     }
 
@@ -48,6 +67,11 @@ class DocsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupWebView()
+        setupListeners()
+    }
+
+    private fun setupWebView() {
         with(mBinding) {
             activity?.onBackPressedDispatcher?.addCallback {
                 if (webView.canGoBack()) {
@@ -56,13 +80,24 @@ class DocsFragment : Fragment() {
                     mActivityViewModel.openHomeScreen()
                 }
             }
-            progressBar.isVisible = true
             with(webView) {
                 webViewClient = mWebViewClient
                 settings.javaScriptEnabled = true
                 settings.domStorageEnabled = true
-                loadUrl("https://pubscale.gitbook.io/offerwall-sdk")
+                launchUrl()
             }
         }
+    }
+
+    private fun setupListeners() {
+        with(mBinding) {
+            swipeToRefresh.setOnRefreshListener {
+                launchUrl()
+            }
+        }
+    }
+
+    private fun launchUrl() {
+        mBinding.webView.loadUrl("https://pubscale.gitbook.io/offerwall-sdk")
     }
 }
