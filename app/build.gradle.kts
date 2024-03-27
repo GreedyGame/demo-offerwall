0110c3da33d0a53eb3218e3da1ad844bc917f718
greedyjustin <justin@greedygame.com>
2024-03-27 13:08:53 +0530

    major: reduced app size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.003"
+        versionName = "1.0.004"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
